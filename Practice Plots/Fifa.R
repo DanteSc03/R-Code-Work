9b5964c0f110c39be09e2d3b18730b4c7d9150f9
Dante Schrantz <149951163+DanteSc03@users.noreply.github.com>
2023-12-18 09:52:08 +0100

    Update Fifa.R

--- Practice Plots/Fifa.R
+++ Practice Plots/Fifa.R
@@ -4,8 +4,13 @@ library(tidyverse)
 women_results <- read.csv("women_results.csv")
 men_results <- read.csv("men_results.csv")
 
-women_filtered <- women_results[as.Date(women_results$date) >= as.Date("2002-01-01"), ]
-men_filtered <- men_results[as.Date(men_results$date) >= as.Date("2002-01-01"), ]
+men_filtered <- men_results %>%
+          filter(tournament == "FIFA World Cup", date > "2002-01-01") %>%
+          mutate(goals_scored = home_score + away_score)
+
+women_filtered <- women_results %>%
+          filter(tournament == "FIFA World Cup", date > "2002-01-01") %>%
+          mutate(goals_scored = home_score + away_score)
 
 women_filtered <- women_filtered %>%
   mutate(goals_scored = home_score + away_score)
@@ -13,9 +18,14 @@ women_filtered <- women_filtered %>%
 men_filtered <- men_filtered %>%
   mutate(goals_scored = home_score + away_score)
 
-t_test_result <- t.test(women_filtered$goals_scored, men_filtered$goals_scored, alternative = "greater", var.equal = TRUE)
+test_results <- wilcox.test(
+  x = women_filtered$goals_scored,
+  y = men_filtered$goals_scored,
+  alternative = "greater"
+)
 
-p_val <- t_test_result$p.value
-result <- ifelse(p_val <= 0.10, "reject", "fail to reject")
+p_val <- round(test_results$p.value, 4)
+result <- ifelse(p_val <= 0.01, "reject", "fail to reject")
+
 
 result_df <- data.frame(p_val, result)
